dc99065b5790a599b79b03ccdd279933a37151db
231189alo <231189@ids.upchiapas.edu.mx>
2024-10-17 11:10:24 -0600

    shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,16 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EditButtonComponent } from './components/edit.button/edit.button.component';
 import { DeleteButtonComponent } from './components/delete.button/delete.button.component';
+import { ComponentTableComponent } from './components/component.table/component.table.component';
 
 
 
 @NgModule({
   declarations: [
+    ComponentTableComponent,
     EditButtonComponent,
     DeleteButtonComponent
   ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    EditButtonComponent,
+    DeleteButtonComponent,
+    ComponentTableComponent
+
   ]
 })
 export class SharedModule { }
